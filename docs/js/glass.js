c408581a1015174a4df771f2b47c032852ba6b9b
David McGlashan <5269729+davidmcglashan@users.noreply.github.com>
2026-05-11 21:57:38 +0100

    Simple drag scrolling

--- docs/js/glass.js
+++ docs/js/glass.js
@@ -1,5 +1,13 @@
 const glass = {
 	elem: null,
+	canvas: null,
+	
+	// Glass is responsible for scroll drags, which we track with this object.
+	drag: {
+		offsetX: 0,
+		offsetY: 0,
+		pressed:false
+	},
 
 	/**
 	 * Initialise the glass pane that sits on top of the UI.
@@ -7,19 +15,47 @@ const glass = {
 	init: () => {
 		// Have the glass listen to mouse events
 		glass.elem = document.getElementById( '-glass' )
+		glass.canvas = document.getElementById( '-canvas' )
 
 		glass.elem.addEventListener( 'mouseup', glass.mouseReleased )
-//		glass.elem.addEventListener( 'mousemove', glass.mouseMoved )
-//		glass.elem.addEventListener( 'mousedown', glass.mousePressed )
+		glass.elem.addEventListener( 'mousemove', glass.mouseMoved )
+		glass.elem.addEventListener( 'mousedown', glass.mousePressed )
+	},
+
+	/**
+	 * the mouse has been pressed
+	 */
+	mousePressed: ( event ) => {
+		glass.drag.x = event.clientX - glass.drag.offsetX
+		glass.drag.y = event.clientY - glass.drag.offsetY
+		glass.drag.pressed = true
+	},
+
+	mouseMoved: ( event ) => {
+		if ( glass.drag.pressed ) {
+			let x = event.clientX - glass.drag.x
+			let y = event.clientY - glass.drag.y
+			glass.canvas.style.transform = `translate(${x}px,${y}px)`
+			glass.drag.moving = true
+		}
 	},
 
 	/**
 	 * The mouse was released. Usually this is a click event.
 	 */
-	mouseReleased: ( event ) => {	
-		let elems = document.elementsFromPoint( event.clientX, event.clientY )
+	mouseReleased: ( event ) => {
+		glass.drag.pressed = false
 		
+		// If this is a drag then let it finish.
+		if ( glass.drag.moving ) {
+			glass.drag.moving = false
+			glass.drag.offsetX = event.clientX - glass.drag.x
+			glass.drag.offsetY = event.clientY - glass.drag.y
+			return
+		}
+
 		// Stop at the first entity and select it
+		let elems = document.elementsFromPoint( event.clientX, event.clientY )
 		for ( let elem of elems ) {
 			if ( elem.classList.contains( 'entity' ) ) {
 				selection.add( elem )
@@ -27,7 +63,7 @@ const glass = {
 			}
 		}
 
-		// Clear the seleciton if the click wasn't on an entity.
+		// Clear the selection if the click wasn't on an entity.
 		selection.clear()
-	}
+	},
 };
--- docs/js/toolbar.js
+++ docs/js/toolbar.js
@@ -1,9 +1,13 @@
 const toolbar = {
 	reset: () => {
 		model.demo()
+		let canvas = document.getElementById( '-canvas' )
+		canvas.style.transform = null
 	},
 
 	new: () => {
 		model.new()
+		let canvas = document.getElementById( '-canvas' )
+		canvas.style.transform = null
 	}
 };
