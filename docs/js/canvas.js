2aea03931099d522b7b9bbc455d135e6618ebe7c
David McGlashan <5269729+davidmcglashan@users.noreply.github.com>
2026-06-02 17:37:13 +0100

    Enter to invokeEditor() might be finally done

--- docs/js/canvas.js
+++ docs/js/canvas.js
@@ -135,16 +135,17 @@ const canvas = {
 		}
 
 		// Enter to invoke the editor
-		// else if ( event.keyCode === 13 && editor.canOpen ) {
-		// 	if ( selection.yes() === 1 ) {
-		// 		// We can pass in an 'event' object based on the selection's location.
-		// 		let rect = selection.first().getBoundingClientRect()
-		// 		editor.invokeEditor({
-		// 			pageX: rect.x+32,
-		// 			pageY: rect.y+16
-		// 		})
-		// 	}
-		// }
+		else if ( event.keyCode === 13 && editor.canOpen ) {
+			if ( selection.yes() === 1 ) {
+				// We can pass in an 'event' object based on the selection's location.
+				let rect = selection.first().getBoundingClientRect()
+				editor.invokeEditor({
+					pageX: rect.x+32,
+					pageY: rect.y+16
+				})
+			}
+			event.preventDefault()
+		}
 
 		// Backspace for delete!
 		else if ( event.keyCode === 8 ) {
