c173a10319b6dc44bce17eea3bdcf0013f318621
David McGlashan <5269729+davidmcglashan@users.noreply.github.com>
2026-07-19 17:56:00 +0100

    Link out to the credits & acks at github.com

--- docs/js/wirehorse.js
+++ docs/js/wirehorse.js
@@ -3,7 +3,7 @@ var wirehorse = {
 	loadCount: 0,
 
 	// The current software version.
-	version: 'v0.5.1',
+	version: 'v0.5.2-latest',
 
 	// These are the scripts that need to be loaded.
 	scripts: [
